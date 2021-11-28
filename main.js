1e67a332af25df20e395cba4a7b29a84fe3f2188
Nicolás Siebert <87218939+nicosiebert@users.noreply.github.com>
2021-11-28 01:32:27 -0300

    update my last junior function xd

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
-const $ = e=>{
-    return document.querySelector(e)
-}
+const $ = (getelement, from=document)=> from.querySelector(getelement);
 
 class $e{
     constructor(url, met, dat){
-    this.url = url
-    this.method =met
-    this.datos = dat
+    this.url = url;
+    this.method =met;
+    this.datos = dat;
     }
     sel(url, met, dat){
-        let datt = new FormData(dat)//aqui va el id de form
+        let datt = new FormData(dat);  //aqui va el id de form
         fetch(url, {method: met, body:datt})
         .then(res => res.text())
         .then(dat => {
-            let arr = JSON.parse(dat)
-            console.log(arr)
-            $('#resp').innerHTML = arr
+            let arr = JSON.parse(dat);
+            console.log(arr);
+            $('#resp').innerHTML = arr;
 
-        })
+        });
     }
 
 }
-var form = $('#form')
+let form = $('#form');
 form.addEventListener('submit', e=>{
-    e.preventDefault()
-    
-    let sl = new $e
-    var ar = sl.sel('crud.php','POST', form)
-    
-    
-
-})
+    e.preventDefault();  
+    let sl = new $e;
+    let ar = sl.sel('crud.php','POST', form);
+});
